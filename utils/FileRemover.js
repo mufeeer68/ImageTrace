223e57e8aa63e41d23fc469e15704ecb5c7bf3c3
mufeer <mufeer.kurukkan@sisgroup.sg>
2020-03-02 14:08:08 +0800

    update

--- utils/FileRemover.js
+++ utils/FileRemover.js
@@ -1,17 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-export const clearDirectory = async () => {
-  let extractedPath = "_" + "complex" + ".jpg" + ".extracted";
-  console.log(extractedPath);
-  if (fs.existsSync(extractedPath)) {
-    console.log("2");
-    fs.readdir(extractedPath, (err, files) => {
-      if (err) console.log(err);
-      console.log("hi");
-      for (const file of files) {
-        console.log(file);
-      }
-    });
+export const clearDirectory = async (req, res) => {
+  try {
+    let filePath = "uploads/simple.gif";
+    let extension = path.parse(filePath).ext;
+    let name = path.parse(filePath).name;
+    let tempname = "uploads/_" + name + extension + ".extracted";
+    const data = [];
+    var textData;
+    if (fs.existsSync(tempname)) {
+      var files = fs.readdirSync(tempname);
+      files.forEach(file => {
+        if (".rar" == path.parse(file).ext) {
+          data.push(tempname + "/" + file);
+        } else if (".zip" == path.parse(file).ext) {
+          data.push(tempname + "/" + file);
+        } else if (".txt" == path.parse(file).ext) {
+          textData = fs.readFileSync(tempname + "/" + file).toString();
+        }
+      });
+    }
+    res.send({ file: data, text: textData });
+  } catch (err) {
+    console.info(err);
+
+    res.status(500).send(err.message);
   }
 };
